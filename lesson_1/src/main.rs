3b28b26cb62f9d2b1146bb0974c94ce07f328aba
GrtlinG0 <adamkural1@gmail.com>
2023-10-05 14:27:13 +0200

    homework_1.1

--- lesson_1/src/main.rs
+++ lesson_1/src/main.rs
@@ -1,3 +1,45 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let unit = get_unit();
+        let temperature = get_temperature();
+        let result = cal_temperature(&unit, &temperature);
+        println!("Converted temperature: {}", result);
+    }
+}
+
+fn get_unit() -> String {
+    let mut unit = String::new();
+    println!("Choose the unit (celsius/fahrenheit):");
+    io::stdin()
+        .read_line(&mut unit)
+        .expect("Failed to read line");
+    unit.trim().to_lowercase() // Convert input to lowercase for comparison
+}
+
+fn get_temperature() -> f64 {
+    let mut temperature = String::new();
+    println!("Enter the temperature:");
+    io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read line");
+    match temperature.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid temperature input. Please try again.");
+            get_temperature() // Recursively call until valid input is provided
+        }
+    }
+}
+
+fn cal_temperature(unit: &str, temperature: &f64) -> f64 {
+    if unit == "celsius" {
+        temperature * 1.8 + 32.0
+    } else if unit == "fahrenheit" {
+        (temperature - 32.0) * 5.0 / 9.0
+    } else {
+        println!("Invalid unit. Assuming Celsius.");
+        temperature * 1.8 + 32.0
+    }
 }
--- lesson_1/target/debug/deps/hello_robot_dreams-3050425e71d8a273.d
+++ lesson_1/target/debug/deps/hello_robot_dreams-3050425e71d8a273.d
@@ -1,5 +1,5 @@
-/home/grtling/Documents/Rust/hello_robot_dreams/target/debug/deps/hello_robot_dreams-3050425e71d8a273: src/main.rs
+/home/grtling/Documents/Rust/hello_robot_dreams/lesson_1/target/debug/deps/hello_robot_dreams-3050425e71d8a273: src/main.rs
 
-/home/grtling/Documents/Rust/hello_robot_dreams/target/debug/deps/hello_robot_dreams-3050425e71d8a273.d: src/main.rs
+/home/grtling/Documents/Rust/hello_robot_dreams/lesson_1/target/debug/deps/hello_robot_dreams-3050425e71d8a273.d: src/main.rs
 
 src/main.rs:
